7c98348f4a8c1d08573edf20192f633a9664bc45
Jornay <lucasdeafg@yahoo.com.br>
2021-04-02 16:17:16 -0300

    add CountDown and newImages about Profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,18 @@
+import styles from '../styles/pages/Profile.module.css';
+
+
 export function Profile(){
     return (
-        <div>
-            <img src="https://github.com/diego3g.png" alt=""/>
+        <div className = {styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/52716819?v=4" alt="Jornay"/>
+            <div>
+                <strong>Eu Lucas</strong> 
+                <p>
+                    <img src="icons/level.svg" alt="level"/>    
+                    Level 1 
+                </p>
+            </div>
         </div>
-        <div>
-            <strong>Diego Testado</strong>
-            <p>Level 1 </p>
-        </div>
+
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,25 @@ import Head from 'next/head';
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 
+import styles from '../styles/pages/Home.module.css';
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
+
 
 export default function Home() {
   return (
-    <div className= "container">
-      
+    <div className= {styles.container}>
+      <Head>
+        <title>Início | Moveit</title>
+      </Head>
+
       <ExperienceBar />
       <section>
         <div>
             <Profile />
-        </div>
+            <CompletedChallenges/> 
+            <Countdown />
+         </div>
         <div>
 
         </div>
