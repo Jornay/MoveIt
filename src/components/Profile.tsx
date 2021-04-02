819c4ba426303b5bdd411ff1851c6e0740cd04af
Jornay <lucasdeafg@yahoo.com.br>
2021-04-02 11:08:17 -0300

    Profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile(){
+    return (
+        <div>
+            <img src="https://github.com/diego3g.png" alt=""/>
+        </div>
+        <div>
+            <strong>Diego Testado</strong>
+            <p>Level 1 </p>
+        </div>
+    );
+}
