ee3b1f56f02151907161020711afea1db80c8c02
Jornay <lucasdeafg@yahoo.com.br>
2021-04-18 17:37:08 -0300

    alteração referente ao inicio do server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
-import express from 'express';
-
+import express, { Request,Response } from 'express';
 const app = express();
+
+app.get("/" , (Request, Response))
 
 app.listen(3334, () => console.log("Server is running!"));
